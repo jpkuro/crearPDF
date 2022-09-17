e9ceb3dcde5e51bb65d9b89938c93f2c775065ca
jpkuro <joseluispalmapiguave@gmail.com>
2022-09-16 23:16:17 -0500

    terminado pdf

--- PDF/Form1.Designer.cs
+++ PDF/Form1.Designer.cs
@@ -28,12 +28,16 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.btnGenerar = new System.Windows.Forms.Button();
+            this.txtFiltro = new System.Windows.Forms.TextBox();
+            this.axAcroPDF = new AxAcroPDFLib.AxAcroPDF();
+            ((System.ComponentModel.ISupportInitialize)(this.axAcroPDF)).BeginInit();
             this.SuspendLayout();
             // 
             // btnGenerar
             // 
-            this.btnGenerar.Location = new System.Drawing.Point(13, 13);
+            this.btnGenerar.Location = new System.Drawing.Point(351, 10);
             this.btnGenerar.Name = "btnGenerar";
             this.btnGenerar.Size = new System.Drawing.Size(75, 23);
             this.btnGenerar.TabIndex = 0;
@@ -41,21 +45,44 @@
             this.btnGenerar.UseVisualStyleBackColor = true;
             this.btnGenerar.Click += new System.EventHandler(this.btnGenerar_Click);
             // 
+            // txtFiltro
+            // 
+            this.txtFiltro.Location = new System.Drawing.Point(38, 10);
+            this.txtFiltro.Name = "txtFiltro";
+            this.txtFiltro.Size = new System.Drawing.Size(236, 20);
+            this.txtFiltro.TabIndex = 1;
+            this.txtFiltro.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // axAcroPDF
+            // 
+            this.axAcroPDF.Enabled = true;
+            this.axAcroPDF.Location = new System.Drawing.Point(27, 51);
+            this.axAcroPDF.Name = "axAcroPDF";
+            this.axAcroPDF.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("axAcroPDF.OcxState")));
+            this.axAcroPDF.Size = new System.Drawing.Size(439, 387);
+            this.axAcroPDF.TabIndex = 2;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(560, 450);
+            this.Controls.Add(this.axAcroPDF);
+            this.Controls.Add(this.txtFiltro);
             this.Controls.Add(this.btnGenerar);
             this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.axAcroPDF)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.Button btnGenerar;
+        private System.Windows.Forms.TextBox txtFiltro;
+        private AxAcroPDFLib.AxAcroPDF axAcroPDF;
     }
 }
 
--- PDF/Form1.cs
+++ PDF/Form1.cs
@@ -1,4 +1,5 @@
 ﻿using iText.IO.Font.Constants;
+using iText.IO.Image;
 using iText.Kernel.Font;
 using iText.Kernel.Geom;
 using iText.Kernel.Pdf;
@@ -27,14 +28,18 @@ namespace PDF
 
         private void btnGenerar_Click(object sender, EventArgs e)
         {
-            creaarPdf();
+            string filtro = txtFiltro.Text;
+            creaarPdf(filtro);
+            axAcroPDF.src = "E:/proyectos visual/PDF/PDF/bin/Debug/ReporteProducto.pdf";
 
         }
-        private void creaarPdf()
+        private void creaarPdf(string filtro)
         {
             PdfWriter pdfWriter = new PdfWriter("reporte.pdf");
             PdfDocument pdf = new PdfDocument(pdfWriter);
-            Document documento = new Document(pdf, PageSize.LETTER);
+            // esto es tamaño carta  Document documento = new Document(pdf, PageSize.LETTER);//
+            PageSize tamanioH = new PageSize(792, 612);
+            Document documento = new Document(pdf, tamanioH);
 
             documento.SetMargins(60, 20, 55, 20);
             PdfFont fontColumnas = PdfFontFactory.CreateFont(StandardFonts.HELVETICA_BOLD);
@@ -51,7 +56,7 @@ namespace PDF
                 tabla.AddHeaderCell(new Cell().Add(new Paragraph(columna).SetFont(fontColumnas)));
             }
 
-            string sql = "SELECT p.codigo, p.nombre, p.precio_publico, p.existencias, c.nombre as categoria FROM productos AS p INNER JOIN categorias AS c ON p.idCategoria=c.id";
+            string sql = "SELECT p.codigo, p.nombre, p.precio_publico, p.existencias, c.nombre as categoria FROM productos AS p INNER JOIN categorias AS c ON p.idCategoria=c.id WHERE c.nombre='" + filtro +"'";
             MySqlConnection conexionBD = conexion.Conexion();
             conexionBD.Open();
 
@@ -60,15 +65,55 @@ namespace PDF
 
             while (reader.Read())
             {
-                tabla.AddCell(new Cell().Add(new Paragraph(reader["codigo"].ToString()).SetFont(fontContenido)));
+                for (int x = 1; x < 100; x++)
+                {
+                   tabla.AddCell(new Cell().Add(new Paragraph(reader["codigo"].ToString()).SetFont(fontContenido)));
                 tabla.AddCell(new Cell().Add(new Paragraph(reader["nombre"].ToString()).SetFont(fontContenido)));
                 tabla.AddCell(new Cell().Add(new Paragraph(reader["precio_publico"].ToString()).SetFont(fontContenido)));
                 tabla.AddCell(new Cell().Add(new Paragraph(reader["existencias"].ToString()).SetFont(fontContenido)));
                 tabla.AddCell(new Cell().Add(new Paragraph(reader["categoria"].ToString()).SetFont(fontContenido)));
+           
+                }
             }
 
             documento.Add(tabla);
             documento.Close();
+
+            var logo = new iText.Layout.Element.Image(ImageDataFactory.Create("C:/Users/jose/Pictures/Saved Pictures/Captura.png")).SetWidth(50);
+            var plogo = new Paragraph("").Add(logo);
+
+            var titulo = new Paragraph("Reporte de productos");
+            titulo.SetTextAlignment(TextAlignment.CENTER);
+            titulo.SetFontSize(12);
+
+            var dfecha =  DateTime.Now.ToString("dd-MM-yyyy");
+            var dhora = DateTime.Now.ToString("hh:mm:ss");
+            var fecha = new Paragraph("Fecha" + dfecha + "\nHora" + dhora);
+            fecha.SetFontSize(12);
+
+            PdfDocument pdfDoc = new PdfDocument(new PdfReader("Reporte.pdf"), new PdfWriter("ReporteProducto.pdf"));
+            Document doc = new Document(pdfDoc);
+
+            int numeros = pdfDoc.GetNumberOfPages();
+
+            for (int i = 1; i <= numeros; i++)
+            {
+                PdfPage pagina = pdfDoc.GetPage(i);
+
+                float y = (pdfDoc.GetPage(i).GetPageSize().GetTop() - 15);
+                doc.ShowTextAligned(plogo, 40, y, i, TextAlignment.CENTER, VerticalAlignment.TOP, 0);
+                doc.ShowTextAligned(titulo, 150, y - 15, i, TextAlignment.CENTER, VerticalAlignment.TOP, 0);
+                doc.ShowTextAligned(fecha, 520, y - 15, i, TextAlignment.CENTER, VerticalAlignment.TOP, 0);
+
+                doc.ShowTextAligned(new Paragraph(String.Format("Pagina {0} de {1}", i, numeros)), pdfDoc.GetPage(i).GetPageSize().GetWidth() / 2, pdfDoc.GetPage(i).GetPageSize().GetBottom() + 30, i, TextAlignment.CENTER, VerticalAlignment.TOP, 0);
+            }
+
+            doc.Close();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
         }
     }
 }
