549282876f6ec4a6d1bf877aff967b422a1b63ed
jpkuro <joseluispalmapiguave@gmail.com>
2022-09-16 21:46:45 -0500

    Agregar archivos de proyecto.

---
+++ PDF/Form1.cs
@@ -0,0 +1,57 @@
+﻿using iText.IO.Font.Constants;
+using iText.Kernel.Font;
+using iText.Kernel.Geom;
+using iText.Kernel.Pdf;
+using iText.Layout;
+using iText.Layout.Element;
+using iText.Layout.Properties;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PDF
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnGenerar_Click(object sender, EventArgs e)
+        {
+            creaarPdf();
+
+        }
+        private void creaarPdf()
+        {
+            PdfWriter pdfWriter = new PdfWriter("reporte.pdf");
+            PdfDocument pdf = new PdfDocument(pdfWriter);
+            Document documento = new Document(pdf, PageSize.LETTER);
+
+            documento.SetMargins(60, 20, 55, 20);
+            PdfFont fontColumnas = PdfFontFactory.CreateFont(StandardFonts.HELVETICA_BOLD);
+            PdfFont fontContenido = PdfFontFactory.CreateFont(StandardFonts.HELVETICA);
+            string[] columnas = {"Codigo","Nombre","Precio","Existencias","Categoria"};
+
+            float[] tamanios = {2, 4, 2, 2, 4 };
+
+            Table tabla = new Table(UnitValue.CreatePercentArray(tamanios));
+            tabla.SetWidth(UnitValue.CreatePercentValue(100));
+
+            foreach (string columna in columnas)
+            {
+                tabla.AddHeaderCell(new Cell().Add(new Paragraph(columna).SetFont(fontColumnas)));
+            }
+
+            documento.Add(tabla);
+            documento.Close();
+        }
+    }
+}
---
+++ PDF/conexion.cs
@@ -0,0 +1,33 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PDF
+{
+    internal class conexion
+    {
+        public static MySqlConnection Conexion()
+        {
+            string servidor = "loclhost";
+            string bd = "tienda";
+            string usuario = "root";
+            string password = "Kuro921*";
+
+            string cadenaConexion = "Database=" + bd + "; Data Source=" + servidor + "; User ID=" + usuario + "; Password=" + password + "";
+
+            try
+            {
+                MySqlConnection conexionDB= new MySqlConnection(cadenaConexion);
+                return conexionDB;
+            }
+            catch (MySqlException ex)
+            {
+                Console.WriteLine("Error al conectarse " + ex.Message);
+                return null;
+            }
+        }
+    }
+}
